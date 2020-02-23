fefe5154ef0eb1308e937e6a1e1dd70dcb07c88a
Riice <riice64@live.co.uk>
2020-02-23 17:46:53 +0100

    Added resource "type" and "IAM" attachment

--- main.tf
+++ main.tf
@@ -19,8 +19,9 @@ resource "aws_iam_user" "git_pull" {
   user_name = "git_pull"
 }
 
-resource "aws_iam_policy" {
+resource "aws_iam_policy" "git_pull_policy" {
     name = "git_pull_policy"
+    user = "{aws_iam_user.git_pull.name}"
     policy = <<EOF
 {
   "Version": "2012-10-17",
